aff42c04b6f2998a5e242635a637b9bd153ab63e
Amal Babu <AMALBABU2027@MCA.AJCE.IN>
2025-10-21 00:00:53 +0530

    payw

--- checkout.php
+++ checkout.php
@@ -47,7 +47,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 // Load Razorpay configuration
-require_once './config/razorpay.php';
+require_once __DIR__ . '/config/razorpay.php';
+
 
 
 $pageTitle = 'Checkout - FreshMart';
